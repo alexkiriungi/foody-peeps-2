ed949226892abc2d6298904d91094fab8ecda3dd
alexkiriungi <mwasxander@gmail.com>
2024-03-04 23:09:58 +0300

    Front-end setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <Button>click me</Button>
+  )
+}
+
+export default App
