ecc286730f8ef6fe0f15b666890fc89a55498912
alexkiriungi <mwasxander@gmail.com>
2024-03-05 10:53:29 +0300

    minor changes

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from 'react-router-dom';
+
+const AppRoutes = () => {
+    return(
+        <Routes>
+            <Route path='/' element={<span>Homme</span>} />
+        </Routes>
+    )
+}
