7af6cac0e2b95e784000b86a9bb809b0730bf06e
alexkiriungi <mwasxander@gmail.com>
2024-03-05 10:45:13 +0300

    Connected to database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL as string).then(() => {
+    console.log("Connected to database successfully");
+})
 
 app.use(express.json());
 app.use(cors());
