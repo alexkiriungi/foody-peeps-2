5e2ee9926931e74643c9a2c6f75bf285e75967e4
alexkiriungi <mwasxander@gmail.com>
2024-03-06 23:11:27 +0300

    minor changes to routes

--- backend/src/routes/myUserRoute.ts
+++ backend/src/routes/myUserRoute.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createMyUser, updateMyUser } from '../controllers/myUserController';
 import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateMyUserrequest } from '../middleware/validation';
 
 const router = express.Router();
 
 router.post('/', jwtCheck, createMyUser);
-router.put('/', jwtCheck, jwtParse, updateMyUser);
+router.put('/', jwtCheck, jwtParse, validateMyUserrequest, updateMyUser);
 
 export default router;
