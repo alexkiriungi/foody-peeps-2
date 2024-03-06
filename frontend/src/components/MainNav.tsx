a75a3c48edbbd60944efd136d3dd3c67b138011f
alexkiriungi <mwasxander@gmail.com>
2024-03-06 09:12:37 +0300

    desktop nav authentication

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,15 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import UsernameMenu from "./UsernameMenu";
 
 
 export default function MainNav() {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
-    <Button 
-    onClick={async () => await loginWithRedirect()}
-    variant='ghost' 
-    className="font-bold hover:text-orange-500 hover:bg-white">
-        Log In
-    </Button>
+    <>
+        <span className="flex space-x-2 items-center">
+            {isAuthenticated ? (<UsernameMenu />) : (
+                <Button 
+                onClick={async () => await loginWithRedirect()}
+                variant='ghost' 
+                className="font-bold hover:text-orange-500 hover:bg-white">
+                    Log In
+                </Button>
+            )}
+        </span>
+    </>
   );
 };
