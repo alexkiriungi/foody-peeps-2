bd0eda928d0cfb7800ac2060fb6fa8895935676d
alexkiriungi <mwasxander@gmail.com>
2024-03-08 23:49:43 +0300

    started Searchpage UI

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -4,8 +4,22 @@ import { useParams } from "react-router-dom"
 
 export default function SearchPage() {
     const { city } = useParams();
-    const { results } = useSearchRestaurants(city);
+    const { results, isLoading } = useSearchRestaurants(city);
+
+    if (!results?.data || !city) {
+        return <span>No results found</span>
+    }
+
+    if (isLoading) {
+        <span>Loading...</span>
+    }
+
   return (
-    <div>SearchPage for {city}</div>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+        <div id="cuisines-list" className="">
+            Insert cuisines here :
+        </div>
+        <div id="main-content" className="flex flex-col gap-5"></div>
+    </div>
   );
 };
