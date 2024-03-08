7299a30a7fde40088b3fa7dc7a2161245b59bcf7
alexkiriungi <mwasxander@gmail.com>
2024-03-08 16:24:49 +0300

    completed fetch restaurant

--- frontend/src/forms/manage-restaurant-form/ImageSection.tsx
+++ frontend/src/forms/manage-restaurant-form/ImageSection.tsx
@@ -1,10 +1,14 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { FormControl, FormDescription, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useFormContext } from "react-hook-form"
 
 
 export default function ImageSection() {
-    const { control } = useFormContext();
+    const { control, watch } = useFormContext();
+
+    const existingImageUrl = watch("imageUrl");
+
   return (
     <div className="space-y-2">
         <div>
@@ -17,7 +21,14 @@ export default function ImageSection() {
                 overwrite the existing one
             </FormDescription>
         </div>
-        <div className="flex flex-col gap-8 w-[50%]">
+        <div className="flex flex-col gap-8 md:w-[50%]">
+            {existingImageUrl && (
+                <AspectRatio ratio={16 / 9}>
+                    <img src={existingImageUrl} 
+                    className="rounded-md object-cover h-full w-full"
+                    alt="restaurant image" />
+                </AspectRatio>
+            )}
             <FormField control={control} name="imageFile"
             render={({ field }) =>(<FormItem>
                 <FormControl>
