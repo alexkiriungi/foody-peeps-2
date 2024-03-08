ccb8b2e1d9b3c3aed5db81f7e3f0d2b847e4ddc3
alexkiriungi <mwasxander@gmail.com>
2024-03-08 15:49:31 +0300

    completed creating a new restaurant

---
+++ frontend/src/api/MyRestaurantApi.tsx
@@ -0,0 +1,38 @@
+import { Restaurant } from "@/types";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation } from "react-query";
+import { toast } from "sonner";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useCreateMyRestaurant = () => {
+    const { getAccessTokenSilently } = useAuth0();
+
+    const createMyRestaurantRequest = async (restaurantFormData: FormData):Promise<Restaurant> => {
+        const accessToken = await getAccessTokenSilently();
+        const res = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            },
+            body: restaurantFormData,
+        });
+
+        if(!res.ok) {
+            throw new Error("Error Failed to create restaurant");
+        }
+
+        return res.json();
+    };
+
+    const { mutate: createRestaurant, isLoading, isSuccess, error} = useMutation(createMyRestaurantRequest);
+
+    if (isSuccess) {
+        toast.success("Restaurant created!")
+    }
+
+    if (error) {
+        toast.error("Unable to update restaurant");
+    }
+    return { createRestaurant, isLoading};
+};
--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,8 +1,10 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 
 export default function ManageRestaurantPage() {
+  const { createRestaurant, isLoading } = useCreateMyRestaurant();
   return (
-    <ManageRestaurantForm />
+    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
   )
 }
