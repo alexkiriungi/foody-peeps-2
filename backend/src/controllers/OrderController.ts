aae97c77a67dbb9073ea15565b0d5f581b429dd0
alexkiriungi <mwasxander@gmail.com>
2024-03-11 13:08:58 +0300

    Completed Order placement

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 import { Request, Response } from 'express';
 import Restaurant, { MenuItemType } from '../models/restaurant';
+import Order from '../models/order';
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
@@ -31,12 +32,20 @@ export const createCheckOutSession = async (req: Request, res: Response) => {
         if (!restaurant) {
             throw new Error("Restaurant not found");
         }
+        const newOrder = new Order({
+            restaurant: restaurant,
+            user: req.userId,
+            status: "placed",
+            deliveryDetails: checkoutSessionRequest.deliveryDetails,
+            cartItems: checkoutSessionRequest.cartItems,
+            createdAt: new Date()
+        })
         
         const lineItems = createLineItems(checkoutSessionRequest, restaurant.menuItems);
 
         const session = await createSession(
             lineItems, 
-            "TEST_ORDER_ID", 
+            newOrder._id.toString(), 
             +restaurant.deliveryPrice,
             restaurant._id.toString()
         );
@@ -44,7 +53,8 @@ export const createCheckOutSession = async (req: Request, res: Response) => {
         if (!session.url) {
             return res.status(500).json({ message: "Error creating stripe session"});
         }
-        
+
+        await newOrder.save()
         res.json({ url: session.url });
     } catch (error: any) {
         console.log(error);
