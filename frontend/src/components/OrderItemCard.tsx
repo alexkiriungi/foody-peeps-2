ec6c32e21ef31286cac15cbfe6a1fea2a4b835eb
alexkiriungi <mwasxander@gmail.com>
2024-03-12 01:15:29 +0300

    completed the Manage dropdown UI

---
+++ frontend/src/components/OrderItemCard.tsx
@@ -0,0 +1,85 @@
+import { Order } from "@/types";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Separator } from "./ui/separator";
+import { Badge } from "./ui/badge";
+import { Label } from "./ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { ORDER_STATUS } from "@/config/order-status-config";
+
+type Props = {
+    order: Order;
+};
+
+const OrderItemCard = ({ order }: Props) => {
+    const getTime = () => {
+        const orderDateTime = new Date(order.createdAt);
+
+        const hours = orderDateTime.getHours();
+        const minutes = orderDateTime.getMinutes();
+
+        const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+        return `${hours}:${paddedMinutes}`;
+    };
+
+    return(
+        <Card>
+            <CardHeader>
+                <CardTitle className="grid md:grid-cols-4 gap-4 justify-between mb-3">
+                    <div className="">
+                        Customer Name:
+                        <span className="ml-2 font-normal">
+                            {order.deliveryDetails.name}
+                        </span>
+                    </div>
+                    <div className="">
+                        Delivery Address:
+                        <span className="ml-2 font-normal italic">
+                            {order.deliveryDetails.addressLine1}, {order.deliveryDetails.city}
+                        </span>
+                    </div>
+                    <div className="">
+                        Time:
+                        <span className="ml-2 font-normal">
+                            {getTime()}
+                        </span>
+                    </div>
+                    <div className="">
+                    Total Cost
+                    <span className="ml-2 font-normal">
+                        ksh{order.totalAmount}
+                    </span>
+                    </div>
+                </CardTitle>
+                <Separator />
+            </CardHeader>
+            <CardContent className="flex flex-col gap-6">
+                <div className="flex flex-col gap-2">
+                    {order.cartItems.map((cartItem) => (
+                        <span>
+                            <Badge variant='outline' className="mr-2">
+                                {cartItem.quantity}
+                            </Badge>
+                            {cartItem.name}
+                        </span>
+                    ))}
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="status">What is the status of this order?</Label>
+                    <Select>
+                        <SelectTrigger id="status">
+                            <SelectValue placeholder="Status" />
+                        </SelectTrigger>
+                        <SelectContent position="popper">
+                            {ORDER_STATUS.map((status) => (
+                                <SelectItem value={status.value}>{status.label}</SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                </div>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default OrderItemCard;
--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,17 +1,36 @@
-import { useCreateMyRestaurant, useGetMyRestaurant, useupdateMyRestaurant } from "@/api/MyRestaurantApi";
+import { useCreateMyRestaurant, useGetMyRestaurant, useGetMyRestaurantOrders, useupdateMyRestaurant } from "@/api/MyRestaurantApi";
+import OrderItemCard from "@/components/OrderItemCard";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+import { TabsContent } from "@radix-ui/react-tabs";
 
 
 export default function ManageRestaurantPage() {
   const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
   const { restaurant } = useGetMyRestaurant();
   const { updateRestaurant, isLoading: isUpdateLoading } = useupdateMyRestaurant();
+  const { orders } = useGetMyRestaurantOrders();
 
   const isEditing =!!restaurant;
   return (
-    <ManageRestaurantForm 
-    onSave={isEditing? updateRestaurant : createRestaurant} 
-    isLoading={isCreateLoading || isUpdateLoading}
-    restaurant={restaurant} />
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent value="orders" className="space-y-5 bg-gray-50 pg-10 rounded-lg">
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order) => (
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+      <ManageRestaurantForm 
+        onSave={isEditing? updateRestaurant : createRestaurant} 
+        isLoading={isCreateLoading || isUpdateLoading}
+        restaurant={restaurant} />
+      </TabsContent>
+    </Tabs>
+    
   );
 };
